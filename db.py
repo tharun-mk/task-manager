aa91ba72e71823b2c5d396aaf0125577bc9c4e47
tharun_mk <mktharun571@gmail.com>
2026-04-13 22:24:27 +0530

    made it secure by making the credentials in .env

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:usha@localhost:5433/tasks"
-engine=create_engine(SQLALCHEMY_DATABASE_URL)
+from dotenv import load_dotenv
+import os
+load_dotenv()
+engine=create_engine(os.getenv(SQLALCHEMY_DATABASE_URL))
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
 def get_db():
