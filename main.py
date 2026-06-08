3d9699ca2dba9413d1f263e705588afb65d020f9
tharun-mk <mktharun571@gmail.com>
2026-06-08 18:38:37 +0530

    changed the origins

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app=FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tharun-mk.github.io/task-manager/","https://tharun-mk.github.io/task-manager","http://127.0.0.1:8000","http://localhost:8000"],
+    allow_origins=["https://tharun-mk.github.io","https://tharun-mk.github.io/task-manager","http://127.0.0.1:8000","http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
